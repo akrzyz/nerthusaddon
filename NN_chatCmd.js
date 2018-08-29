1313fa04375ec42ff3128885a3a8b6524afb3a90
KrisAphalon <Krzysztof.Kacz@interia.pl>
2018-08-29 11:07:17 +0200

    Changed *map and *addGraf to be one map only
    Also: fixed typos and updated so now it also logs map on which commands were executed. Additionally changed code for *map to create additional <img> instead of changing url of backgroundImage and added fallback when you invoke *light command without number (then it resets like *map so it's consistent)
    
    "first unnamed map" is a phenomenon which occurs when there is no messages on chat on loading the game. Game then doesn't send name of map on which you spawned, so easy implementation falls short here, because commands directly on nameless map would be seen on all maps, which is bad in case of *addGraf and *map
    
    New code uses sessionStorage to store timestamps of messages connected to maps on which they were executed. sessionStorage is supported up to IE8 and it's so huge there is no need to check if it's available, as there is almost no chance users would run this code on such old browser (worst case scenario first "unnamed" map can be buggy). Cookies are out of option in this case due to relatively big size of data so sessionStorage is way to go.
    
    There is one rare case I'm aware of in which this code might not work (I haven't strictly tested it):
    user sees command on map A
    user goes to map B
    user quits web browser, but then immediately logs back, so that sessionStorage is deleted but chat doesn't recognize him as loged off to clear chat.
    Then user will see objects from map A on map B
    
    Fixing this issue would presumably require creating cookie on closing the page with extremely low expiration time (10s-3min, exact timing must be tested) and then on new load checking if it's set. It requires deep interference and testing and I don't think it's necessary to do it (at least for now).

--- NN_chatCmd.js
+++ NN_chatCmd.js
@@ -3,6 +3,7 @@ try{
 nerthus.chatCmd = {};
 nerthus.chatCmd.map = {};
 nerthus.chatCmd.public_map = {};
+nerthus.chatCmd.current_map = '';
 
 nerthus.chatCmd.run = function(ch)
 {
@@ -12,17 +13,28 @@ nerthus.chatCmd.run = function(ch)
         var callback = this.fetch_callback(cmd,ch)
         if(callback)
         {
-            log("["+ch.k+"] " + ch.n + " -> " + ch.t) //gdze kto co
-            return callback(ch)
+            log("["+ch.k+"] " + ch.n + " -> " + ch.t +
+            " (" + nerthus.chatCmd.current_map +")"); //gdzie kto co mapa
+            return callback(ch);
         }
     }
 }
 
 nerthus.chatCmd.fetch_cmd = function(ch)
 {
-    if(ch.t[0]=='*')
-        return RegExp(/^\*(\S+)/).exec(ch.t)
-    return null
+    if (nerthus.chatCmd.currentMap === '')
+        nerthus.chatCmd.currentMap = map.name.replace(/\s/g, '');
+    if (ch.s === 'entertown')
+    {
+        $('#_ng-map-' + nerthus.chatCmd.currentMap).hide();
+        $('._ng-' + nerthus.chatCmd.currentMap).hide();
+        nerthus.chatCmd.currentMap = ch.t.replace(/\s/g, '');
+        $('#_ng-map-' + nerthus.chatCmd.currentMap).show();
+        $('._ng-' + nerthus.chatCmd.currentMap).show();
+    } 
+    else if(ch.t[0]=='*')
+        return RegExp(/^\*(\S+)/).exec(ch.t);
+    return null;
 }
 
 nerthus.chatCmd.fetch_callback = function(cmd,ch)
@@ -99,33 +111,56 @@ nerthus.chatCmd.map["map"] = function(ch)
 {
     var map_url = ch.t.split(" ").slice(1).join(" ");
     map_url = nerthus.chatCmd.extractUrlFromDecorator(map_url);
-    $("#ground").css("backgroundImage","url(" + map_url + ")")
+    var map_container = $("#_ng-map-" + nerthus.chatCmd.currentMap);
+    if (map_container.length)
+        map_container.css("backgroundImage", "url(" + map_url + ")");
+    else
+    {
+        $("<img id='_ng-map-" + nerthus.chatCmd.currentMap + "'>")
+        .css({
+            "backgroundImage": "url(" + map_url + ")",
+            "width": map.x << 5,
+            "height": map.y << 5,
+            "position": "absolute"
+        }).appendTo("#ground");
+    }
     return true;
 }
 
 nerthus.chatCmd.map["light"] = function(ch)
 {
     var opacity = ch.t.split(" ")[1];
+    if (opacity === undefined)
+        opacity = "";
     $("#base").css("opacity",opacity);
     return true;
 }
 
 nerthus.chatCmd.map["addGraf"] = function(ch)
 {  //cmd[0]=x, cmd[1]=y, cmd[2]=url, cmd[3]=tip_text, cmd[4]=isCol
-    var cmd = ch.t.split(" ").slice(1).join(" ").split(",");
-    var x = parseInt(cmd[0]);
-    var y = parseInt(cmd[1]);
-    var _url = nerthus.chatCmd.extractUrlFromDecorator(cmd[2]);
-    var _tip = cmd[3] ? ' tip="<b>'+cmd[3]+'</b>" ctip="t_npc"' : "";
-    var isCol = parseInt(cmd[4]);
-    $('<img id="_ng-' + cmd[0] + '-' + cmd[1] + '" src="'+ _url +'"'+_tip+'>').css("position","absolute").appendTo('#base')
-    .load(function()
-    {  //wyśrodkowanie w osi x i wyrównanie do stóp w osi y
-        var _x = 32 * x + 16 - Math.floor($(this).width() / 2);
-        var _y = 32 * y + 32 - $(this).height();
-        $(this).css({top:"" + _y + "px", left: "" + _x + "px"}).css("z-index", y * 2 + 9);
-    })
-    if(isCol) g.npccol[ x + 256 * y] = true;
+    var map_ts = sessionStorage.getItem(ch.ts); //Test for 1st "unnamed" map
+    if (map_ts === null || map_ts === nerthus.chatCmd.currentMap || map_ts === undefined) 
+    {
+        var cmd = ch.t.split(" ").slice(1).join(" ").split(",");
+        var x = parseInt(cmd[0]);
+        var y = parseInt(cmd[1]);
+        var _url = nerthus.chatCmd.extractUrlFromDecorator(cmd[2]);
+        var _tip = cmd[3] ? ' tip="<b>'+cmd[3]+'</b>" ctip="t_npc"' : "";
+        var isCol = parseInt(cmd[4]);
+        $('<img id="_ng-' + cmd[0] + '-' + cmd[1] + '" src="'+ _url +'"'+_tip+'>')
+        .addClass('_ng-' + nerthus.chatCmd.currentMap)
+        .css("position","absolute")
+        .appendTo('#base')
+        .load(function()
+        {  //wyśrodkowanie w osi x i wyrównanie do stóp w osi y
+            var _x = 32 * x + 16 - Math.floor($(this).width() / 2);
+            var _y = 32 * y + 32 - $(this).height();
+            $(this).css({top:"" + _y + "px", left: "" + _x + "px", "z-index": y * 2 + 9})
+        });
+        if(isCol) g.npccol[ x + 256 * y] = true;
+        if (map_ts === null)
+            sessionStorage.setItem(ch.ts, nerthus.chatCmd.currentMap);
+    }
     return true;
 }
 
@@ -173,7 +208,7 @@ nerthus.chatCmd.public_map["me"] = function(ch)
 
 nerthus.chatCmd.start = function()
 {
-    //style do dialogow
+    //style do dialogów
     $("<style type='text/css'> #chattxt .nar2{ color:#D6A2FF ; } </style>").appendTo("head");
     $("<style type='text/css'> #chattxt .nar3{ color:#00CED1 ; } </style>").appendTo("head");
     $("<style type='text/css'> #chattxt .dial1{ color:#33CC66 ; } </style>").appendTo("head");
