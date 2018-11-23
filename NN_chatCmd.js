a7898aa0c759c30ec0e0d87dd79ebad2f2f5db69
KrisAphalon <Krzysztof.Kacz@interia.pl>
2018-11-23 19:17:58 +0100

    Reverse changes to NN_chatCmd

--- NN_chatCmd.js
+++ NN_chatCmd.js
@@ -3,7 +3,6 @@ try{
 nerthus.chatCmd = {};
 nerthus.chatCmd.map = {};
 nerthus.chatCmd.public_map = {};
-nerthus.chatCmd.current_map = '';
 
 nerthus.chatCmd.run = function(ch)
 {
@@ -13,28 +12,17 @@ nerthus.chatCmd.run = function(ch)
         var callback = this.fetch_callback(cmd,ch)
         if(callback)
         {
-            log("["+ch.k+"] " + ch.n + " -> " + ch.t +
-            " (" + nerthus.chatCmd.current_map +")"); //gdzie kto co mapa
-            return callback(ch);
+            log("["+ch.k+"] " + ch.n + " -> " + ch.t) //gdze kto co
+            return callback(ch)
         }
     }
 }
 
 nerthus.chatCmd.fetch_cmd = function(ch)
 {
-    if (nerthus.chatCmd.currentMap === '')
-        nerthus.chatCmd.currentMap = map.name.replace(/\s/g, '');
-    if (ch.s === 'entertown')
-    {
-        $('#_ng-map-' + nerthus.chatCmd.currentMap).hide();
-        $('._ng-' + nerthus.chatCmd.currentMap).hide();
-        nerthus.chatCmd.currentMap = ch.t.replace(/\s/g, '');
-        $('#_ng-map-' + nerthus.chatCmd.currentMap).show();
-        $('._ng-' + nerthus.chatCmd.currentMap).show();
-    } 
-    else if(ch.t[0]=='*')
-        return RegExp(/^\*(\S+)/).exec(ch.t);
-    return null;
+    if(ch.t[0]=='*')
+        return RegExp(/^\*(\S+)/).exec(ch.t)
+    return null
 }
 
 nerthus.chatCmd.fetch_callback = function(cmd,ch)
@@ -96,8 +84,8 @@ nerthus.chatCmd.map["dial666"] = function(ch)
 
 nerthus.chatCmd.makeDialogTextWithSpeaker = function(str)
 {
-   str = str.split(" ").slice(1).join(" ").split(",");
-   return "«"+str[0]+"» " + str.slice(1).join(",");
+    str = str.split(" ").slice(1).join(" ").split(",");
+    return "«"+str[0]+"» " + str.slice(1).join(",");
 }
 
 nerthus.chatCmd.map["sys"] = function(ch)
@@ -111,56 +99,33 @@ nerthus.chatCmd.map["map"] = function(ch)
 {
     var map_url = ch.t.split(" ").slice(1).join(" ");
     map_url = nerthus.chatCmd.extractUrlFromDecorator(map_url);
-    var map_container = $("#_ng-map-" + nerthus.chatCmd.currentMap);
-    if (map_container.length)
-        map_container.css("backgroundImage", "url(" + map_url + ")");
-    else
-    {
-        $("<img id='_ng-map-" + nerthus.chatCmd.currentMap + "'>")
-        .css({
-            "backgroundImage": "url(" + map_url + ")",
-            "width": map.x << 5,
-            "height": map.y << 5,
-            "position": "absolute"
-        }).appendTo("#ground");
-    }
+    $("#ground").css("backgroundImage","url(" + map_url + ")")
     return true;
 }
 
 nerthus.chatCmd.map["light"] = function(ch)
 {
     var opacity = ch.t.split(" ")[1];
-    if (opacity === undefined)
-        opacity = "";
     $("#base").css("opacity",opacity);
     return true;
 }
 
 nerthus.chatCmd.map["addGraf"] = function(ch)
 {  //cmd[0]=x, cmd[1]=y, cmd[2]=url, cmd[3]=tip_text, cmd[4]=isCol
-    var map_ts = sessionStorage.getItem(ch.ts); //Test for 1st "unnamed" map
-    if (map_ts === null || map_ts === nerthus.chatCmd.currentMap || map_ts === undefined) 
-    {
-        var cmd = ch.t.split(" ").slice(1).join(" ").split(",");
-        var x = parseInt(cmd[0]);
-        var y = parseInt(cmd[1]);
-        var _url = nerthus.chatCmd.extractUrlFromDecorator(cmd[2]);
-        var _tip = cmd[3] ? ' tip="<b>'+cmd[3]+'</b>" ctip="t_npc"' : "";
-        var isCol = parseInt(cmd[4]);
-        $('<img id="_ng-' + cmd[0] + '-' + cmd[1] + '" src="'+ _url +'"'+_tip+'>')
-        .addClass('_ng-' + nerthus.chatCmd.currentMap)
-        .css("position","absolute")
-        .appendTo('#base')
+    var cmd = ch.t.split(" ").slice(1).join(" ").split(",");
+    var x = parseInt(cmd[0]);
+    var y = parseInt(cmd[1]);
+    var _url = nerthus.chatCmd.extractUrlFromDecorator(cmd[2]);
+    var _tip = cmd[3] ? ' tip="<b>'+cmd[3]+'</b>" ctip="t_npc"' : "";
+    var isCol = parseInt(cmd[4]);
+    $('<img id="_ng-' + cmd[0] + '-' + cmd[1] + '" src="'+ _url +'"'+_tip+'>').css("position","absolute").appendTo('#base')
         .load(function()
         {  //wyśrodkowanie w osi x i wyrównanie do stóp w osi y
             var _x = 32 * x + 16 - Math.floor($(this).width() / 2);
             var _y = 32 * y + 32 - $(this).height();
-            $(this).css({top:"" + _y + "px", left: "" + _x + "px", "z-index": y * 2 + 9})
-        });
-        if(isCol) g.npccol[ x + 256 * y] = true;
-        if (map_ts === null)
-            sessionStorage.setItem(ch.ts, nerthus.chatCmd.currentMap);
-    }
+            $(this).css({top:"" + _y + "px", left: "" + _x + "px"}).css("z-index", y * 2 + 9);
+        })
+    if(isCol) g.npccol[ x + 256 * y] = true;
     return true;
 }
 
